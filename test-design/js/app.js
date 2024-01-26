152454b6c0fe6005f91b83e752556ea0142d7b96
Muhammad Hashir <37501188+hashirm@users.noreply.github.com>
2024-01-26 22:13:56 +0500

    added test design

---
+++ test-design/js/app.js
@@ -0,0 +1,10 @@
+$("nav .menu-flexCont-02 .menu-icon").click(function(){
+
+    $(".mobile-menu").css("transform","translateY(0)");
+
+})
+$(".mobile-menu .mobile-flexCont .close-icon").click(function(){
+    
+    $(".mobile-menu").css("transform", "translateY(-510px)");
+
+})
