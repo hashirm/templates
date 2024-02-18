5730dbbc6317ed4ba229ce4bc52cb96a05fd4ea4
Muhammad Hashir <37501188+hashirm@users.noreply.github.com>
2024-02-18 15:38:00 +0500

    updated

--- Newrich/script.js
+++ Newrich/script.js
@@ -30,20 +30,67 @@ navlinks.forEach((link) => {
     console.log(
       e.target.parentElement.querySelector("ul").classList.toggle("show1")
     );
-
-    // e.target.closest("ul").classList.toggle("show1");
   });
 });
 
-// let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-// htmlcssArrow.onclick = function () {
-//   navLinks.classList.toggle("show1");
-// };
-// let moreArrow = document.querySelector(".more-arrow");
-// moreArrow.onclick = function () {
-//   navLinks.classList.toggle("show2");
-// };
-// let jsArrow = document.querySelector(".js-arrow");
-// jsArrow.onclick = function () {
-//   navLinks.classList.toggle("show3");
-// };
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        label: ["# of Votes"],
+        data: [2.5, 3.5, 5.5, 4.5, 5, 7.5],
+        backgroundColor: [
+          "rgba(255, 84, 62, 0.2)",
+          "rgba(30, 153, 204, 0.2)",
+          "rgba(46,198,163,0.2)",
+          "rgba(107, 3, 137, 0.2)",
+          "rgba(255, 222, 85, 0.2)",
+          "rgba(110, 111, 128, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        barThickness: 115,
+        borderColor: [
+          "rgb(255, 84, 62)",
+          "rgb(30, 153, 204)",
+          "rgb(46,198,163)",
+          "rgb(107, 3, 137)",
+          "rgb(255, 222, 85)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      labels: {
+        render: "label",
+        fontColor: "#000",
+        fontSize: 16,
+        fontWeight: "bold",
+        fontFamily: "'Poppins', sans-serif",
+        position: "top",
+      },
+    },
+    scales: {
+      y: {
+        ticks: {
+          suggestedMin: 10,
+          callback: function (value, index, values) {
+            return "$" + value * 100; // Add a dollar sign before the value
+          },
+        },
+      },
+    },
+  },
+});
+
+const canvas = document.querySelector("canvas");
+console.log(canvas);
