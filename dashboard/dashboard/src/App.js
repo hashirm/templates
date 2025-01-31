79260748b75b146e808e0eb00093412f02bc25fa
Muhammad Hashir <m.hashir@applab.qa>
2025-01-31 13:13:09 +0500

    added contacts and invoices and form

--- dashboard/dashboard/src/App.js
+++ dashboard/dashboard/src/App.js
@@ -5,10 +5,10 @@ import { Routes, Route } from "react-router-dom";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team/index";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
 // import Bar from "./scenes/bar";
-// import form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
@@ -29,9 +29,9 @@ function App() {
               <Routes>
                 <Route path="/" element={<Dashboard/>} />
                 <Route path="/team" element={<Team/>} />
-                {/* <Route path="/contacts" element={<Contacts/>} /> */}
-                {/* <Route path="/invoices" element={<Invoices/>} /> */}
-                {/* <Route path="/form" element={<Form/>} /> */}
+                <Route path="/contacts" element={<Contacts/>} />
+                <Route path="/invoices" element={<Invoices/>} />
+                <Route path="/form" element={<Form/>} />
                 {/* <Route path="/bar" element={<Bar/>} /> */}
                 {/* <Route path="/pie" element={<Pie/>} /> */}
                 {/* <Route path="/line" element={<Line/>} /> */}
